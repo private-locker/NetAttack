75dc844491dbfae47f0d48a5723e5128f2c19b57
REDD <ar1k88gamingllc@gmail.com>
2021-02-05 21:38:47 -0800

    Delete var/www/html directory

--- var/www/html/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<meta http-equiv="Cache-Control" content="no-cache" />
-<meta http-equiv="refresh" content="0; URL=/cgi-bin/core.sh" />
-</head>
-<body style="background-color:black">
-<a style="color: white; font-family: arial, verdana;" href="/cgi-bin/core.sh">NetAttack UI</a>
-</body>
-</html>
