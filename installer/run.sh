48bcd4501c9b00e9280f0489b2d5977842cbf1d6
REDD <ar1k88gamingllc@gmail.com>
2021-02-06 01:55:29 -0800

    Update run.sh

--- installer/run.sh
+++ installer/run.sh
@@ -4,12 +4,6 @@ sudo apt-get update -y
 sudo apt-get full-upgrade -y
 sudo apt-get install software-properties-common build-essential -y
 sudo apt-get install dhcpcd5 lighttpd git hostapd dnsmasq iptables-persistent vnstat qrencode php7.4-cgi dkms libelf-dev hcxtools hcxdumptool -y
-cd
-git clone https://github.com/aircrack-ng/rtl8812au.git
-cd rtl8812au
-sed -i 's/CONFIG_PLATFORM_I386_PC = y/CONFIG_PLATFORM_I386_PC = n/g' Makefile
-sed -i 's/CONFIG_PLATFORM_ARM64_RPI = n/CONFIG_PLATFORM_ARM64_RPI = y/g' Makefile
-make && make install
 
 sudo systemctl enable lighttpd.service
 sudo systemctl enable dhcpcd.service
@@ -24,22 +18,10 @@ echo "net.ipv4.ip_forward=1" | sudo tee /etc/sysctl.d/99-sysctl.conf > /dev/null
 sudo sysctl -p /etc/sysctl.d/99-sysctl.conf 
 sudo /etc/init.d/procps restart
 
-printf "[NetDev]
-Name=br0
-Kind=bridge
-" > /etc/systemd/network/bridge-br0.netdev
-
-printf "
-[Match]
-Name=eth0
-
-[Network]
-Bridge=br0
-" > /etc/systemd/network/br0-member-eth0.network
-
 
 systemctl enable systemd-networkd
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 iptables-save | sudo tee /etc/iptables/rules.v4
 systemctl unmask hostapd.service
 systemctl enable hostapd.service
@@ -56,7 +38,6 @@ channel=1
 hw_mode=g
 wpa_passphrase=ChangeMe
 interface=wlan0
-bridge=br0
 wpa=2
 wpa_pairwise=CCMP
 country_code=
@@ -83,18 +64,27 @@ slaac private
 nohook lookup-hostname
 
 # NetAttack wlan0 configuration
-#denyinterfaces wlan0 eth0
 interface wlan0
-#interface br0
 static ip_address=10.3.141.1/24
 static routers=10.3.141.1
 static domain_name_server=8.8.8.8 8.8.4.4
 " > /etc/dhcpcd.conf
 sudo systemctl umask lighttpd
 sudo systemctl enable lighttpd
+
+wget https://raw.githubusercontent.com/private-locker/NetAttack/main/installer/lighttpd.conf -O lighttpd.conf
 cat ${PWD}/lighttpd.conf > /etc/lighttpd/lighttpd.conf
+rm lighttpd.conf
+
+cd
+git clone https://github.com/aircrack-ng/rtl8812au.git
+cd rtl8812au
+sed -i 's/CONFIG_PLATFORM_I386_PC = y/CONFIG_PLATFORM_I386_PC = n/g' Makefile
+sed -i 's/CONFIG_PLATFORM_ARM64_RPI = n/CONFIG_PLATFORM_ARM64_RPI = y/g' Makefile
+make && make install
+cd
 
 sudo systemctl restart dhcpcd
 sudo systemctl enable dnsmasq
 sudo systemctl enable hostapd
-sudo systemctl reboot
+sudo reboot now
