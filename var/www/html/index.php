75c5d60a45c08e7d1b8250aef250cd678e7b7556
REDD <ar1k88gamingllc@gmail.com>
2021-02-05 21:37:34 -0800

    Create index.php

---
+++ var/www/html/index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<meta http-equiv="Cache-Control" content="no-cache" />
+<meta http-equiv="refresh" content="0; URL=/cgi-bin/core.sh" />
+</head>
+<body style="background-color:black">
+<a style="color: white; font-family: arial, verdana;" href="/cgi-bin/core.sh">NetAttack UI</a>
+</body>
+</html>
