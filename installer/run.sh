0002760572cf7ed32772093a326abe2cdfd653c8
REDD <ar1k88gamingllc@gmail.com>
2021-02-06 21:43:15 -0800

    Create run.sh

--- installer/run.sh
+++ installer/run.sh
@@ -22,6 +22,8 @@ sudo /etc/init.d/procps restart
 systemctl enable systemd-networkd
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+iptables -A FORWARD -i wlan0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
+iptables -A FORWARD -i eth0 -o wlan0 -j ACCEPT
 iptables-save | sudo tee /etc/iptables/rules.v4
 systemctl unmask hostapd.service
 systemctl enable hostapd.service
